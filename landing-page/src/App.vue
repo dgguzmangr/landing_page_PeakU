3f8210374c1e0ebfcc1b6df7746b1fb272e7bb0f
dgguzmangr <dgguzmangr@gmail.com>
2023-03-27 21:45:56 -0500

    sections 1 to 4 creation

--- landing-page/src/App.vue
+++ landing-page/src/App.vue
@@ -2,16 +2,29 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Section1nav/>
+    <Section2/>
+    <Section3/>
+    <section4carrousel/>
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import section1nav from './components/section1-nav.vue'
+import section2 from './components/section2.vue'
+import section3 from './components/section3.vue'
+import section4carrousel from './components/section4-carrousel.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Section1nav,
+    Section2,
+    Section3,
+    Section4carrousel
   }
 }
 </script>
